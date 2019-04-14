9f4ee1501bd625321d9a8010987818a58eee1c1d
anthrdreem <slvault101@gmail.com>
2019-04-14 23:35:19 +1000

    PLS

---
+++ Pizza/Models/AppUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Pizza.Models
+{
+    public class AppUser : IdentityUser
+    {        // no additional members are required      
+             // for basic Identity installation    
+    }
+}
